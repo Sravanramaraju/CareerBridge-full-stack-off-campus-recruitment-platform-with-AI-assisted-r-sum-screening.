d983d5e68c9aa92c8f0a40b836c363d1a14224e3
Sravan-ramaraju <sravantatikonda123@gmail.com>
2026-09-09 15:36:02 +0530

    feat: run email outbox worker with API lifecycle

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,14 @@ import { createApp } from './app.js';
 import { env } from './config/env.js';
 import { disconnectDatabase } from './lib/database.js';
 import { logger } from './lib/logger.js';
+import { emailWorker } from './modules/email/emailWorker.js';
 import { checkDatabase } from './modules/health/health.service.js';
 
 const server = createServer(createApp({ databaseCheck: checkDatabase }));
 
 server.listen(env.PORT, () => {
   logger.info({ port: env.PORT, environment: env.NODE_ENV }, 'CareerBridge API listening');
+  emailWorker.start();
 });
 
 function shutdown(signal) {
@@ -19,6 +21,7 @@ function shutdown(signal) {
       process.exitCode = 1;
     }
 
+    await emailWorker.stop();
     await disconnectDatabase();
   });
 }
